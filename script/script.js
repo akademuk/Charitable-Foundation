ceeb1062a00711e75aba96a894225fe799af8afe
Oleg <of@weblife.ua>
2024-05-31 10:03:58 +0300

    add slider

--- script/script.js
+++ script/script.js
@@ -139,7 +139,7 @@ function initDropdownMenu() {
         if (isAnimating) return;
 
         isAnimating = true;
-        const duration = 200;
+        const duration = 100;
 
         if (dropdownMenu.hasClass('show')) {
             hideDropdownItems(dropdownItems, duration, function () {
@@ -161,7 +161,7 @@ function initDropdownMenu() {
         if (!$(e.target).closest('.header__dropdown--toggle, .header__dropdown--menu').length) {
             if (dropdownMenu.hasClass('show') && !isAnimating) {
                 isAnimating = true;
-                const duration = 200;
+                const duration = 100;
 
                 hideDropdownItems(dropdownItems, duration, function () {
                     dropdownMenu.removeClass('show');
@@ -176,7 +176,7 @@ function initDropdownMenu() {
     $(window).on('keydown', function (e) {
         if (e.key === 'Escape' && dropdownMenu.hasClass('show') && !isAnimating) {
             isAnimating = true;
-            const duration = 200;
+            const duration = 100;
 
             hideDropdownItems(dropdownItems, duration, function () {
                 dropdownMenu.removeClass('show');
@@ -289,6 +289,27 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 document.addEventListener('DOMContentLoaded', function () {
+    const swiper3 = new Swiper('.partner__slider', {
+        slidesPerView: 'auto',
+        spaceBetween: 8 ,
+        pagination: {
+            el: '.partner__pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.partner__next',
+            prevEl: '.partner__prev',
+        },
+        breakpoints: {
+            // when window width is >= 320px
+            1280: {
+                spaceBetween: 24,
+            },
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
     const swiper2 = new Swiper('.accordion__content--slider2', {
         slidesPerView: 'auto',
         spaceBetween: 16,
@@ -303,20 +324,162 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const headers = document.querySelectorAll('.accordion__header');
 
     headers.forEach(header => {
-        header.addEventListener('click', function() {
+        header.addEventListener('click', function () {
             const activeContent = document.querySelector('.accordion__content--active');
+            const activeHeader = document.querySelector('.accordion__header.active');
 
             if (activeContent && activeContent !== this.nextElementSibling) {
                 activeContent.classList.remove('accordion__content--active');
             }
 
+            if (activeHeader && activeHeader !== this) {
+                activeHeader.classList.remove('active');
+            }
+
             const content = this.nextElementSibling;
             content.classList.toggle('accordion__content--active');
+            if (content.classList.contains('accordion__content--active')) {
+                this.classList.add('active');
+            } else {
+                this.classList.remove('active');
+            }
+        });
+    });
+
+    const galleryCards = document.querySelectorAll('.gallery__box--left-bottom-card, .gallery__box--left-bottom-card2');
+
+    galleryCards.forEach(card => {
+        card.addEventListener('click', function () {
+            const targetId = this.getAttribute('data-target');
+            const targetAccordionItem = document.getElementById(targetId);
+
+            if (targetAccordionItem) {
+                // Закрываем все открытые аккордеоны и удаляем класс active с заголовков
+                const activeContents = document.querySelectorAll('.accordion__content--active');
+                const activeHeaders = document.querySelectorAll('.accordion__header.active');
+                activeContents.forEach(content => {
+                    content.classList.remove('accordion__content--active');
+                });
+                activeHeaders.forEach(header => {
+                    header.classList.remove('active');
+                });
+
+                // Прокручиваем к нужному блоку
+                targetAccordionItem.scrollIntoView({ behavior: 'smooth' });
+
+                // Раскрываем нужный блок аккордеона и добавляем класс active заголовку
+                const header = targetAccordionItem.querySelector('.accordion__header');
+                const content = targetAccordionItem.querySelector('.accordion__content');
+                content.classList.add('accordion__content--active');
+                header.classList.add('active');
+            }
         });
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const swiperSlides = document.querySelectorAll('.swiper-slide--open');
+    const subMenuOverlay = document.getElementById('becomeOurPartner');
+    const body = document.body;
+
+    if (swiperSlides.length && subMenuOverlay) {
+        swiperSlides.forEach(swiperSlide => {
+            swiperSlide.addEventListener('click', function () {
+                subMenuOverlay.classList.toggle('open');
+                body.classList.toggle('active');
+            });
+        });
+
+        // Закрытие при нажатии клавиши ESC
+        document.addEventListener('keydown', function (event) {
+            if (event.key === 'Escape' || event.key === 'Esc') {
+                subMenuOverlay.classList.remove('open');
+                body.classList.remove('active');
+            }
+        });
+
+        // Закрытие при клике вне области блока
+        document.addEventListener('click', function (event) {
+            if (!subMenuOverlay.contains(event.target) && !Array.from(swiperSlides).some(slide => slide.contains(event.target))) {
+                subMenuOverlay.classList.remove('open');
+                body.classList.remove('active');
+            }
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const donate = document.querySelectorAll('.donate');
+    const makeDonation = document.getElementById('makeDonation');
+    const body = document.body;
+
+    if (donate.length && makeDonation) {
+        donate.forEach(donateButton => {
+            donateButton.addEventListener('click', function (event) {
+                event.stopPropagation(); // Остановить всплытие события
+                makeDonation.classList.toggle('open');
+                body.classList.toggle('active');
+            });
+        });
+
+        // Закрытие при нажатии клавиши ESC
+        document.addEventListener('keydown', function (event) {
+            if (event.key === 'Escape' || event.key === 'Esc') {
+                makeDonation.classList.remove('open');
+                body.classList.remove('active');
+            }
+        });
+
+        // Закрытие при клике вне области блока
+        document.addEventListener('click', function (event) {
+            if (!makeDonation.contains(event.target) && !Array.from(donate).some(button => button.contains(event.target))) {
+                makeDonation.classList.remove('open');
+                body.classList.remove('active');
+            }
+        });
+
+        // Остановить всплытие клика внутри makeDonation
+        makeDonation.addEventListener('click', function(event) {
+            event.stopPropagation();
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const partnerBtn = document.querySelectorAll('.partner-btn');
+    const becomeOurPartner = document.getElementById('becomeOurPartner');
+    const body = document.body;
+
+    if (partnerBtn.length && becomeOurPartner) {
+        partnerBtn.forEach(donateButton => {
+            donateButton.addEventListener('click', function (event) {
+                event.stopPropagation(); 
+                becomeOurPartner.classList.toggle('open');
+                body.classList.toggle('active');
+            });
+        });
+
+        document.addEventListener('keydown', function (event) {
+            if (event.key === 'Escape' || event.key === 'Esc') {
+                becomeOurPartner.classList.remove('open');
+                body.classList.remove('active');
+            }
+        });
+
+        document.addEventListener('click', function (event) {
+            if (!becomeOurPartner.contains(event.target) && !Array.from(partnerBtn).some(button => button.contains(event.target))) {
+                becomeOurPartner.classList.remove('open');
+                body.classList.remove('active');
+            }
+        });
+
+        becomeOurPartner.addEventListener('click', function(event) {
+            event.stopPropagation();
+        });
+    }
+});
+
